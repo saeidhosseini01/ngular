b3e1caaa937e34b33d39c60a434ab8989f1a7ace
SeyedsaeidHosseini <SeyedsaeidHosseini1990@gmail.com>
2025-03-15 00:56:12 +0330

    observ

--- serviceTest/src/app/home/home.component.ts
+++ serviceTest/src/app/home/home.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatTabsModule } from '@angular/material/tabs';
+import { Observable } from 'rxjs';
+import { TODO } from '../todo';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-home',
-  imports: [ MatTabsModule],
+  imports: [MatTabsModule,CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+
+
+
+
+export class HomeComponent implements OnInit {
+  public http$?: Observable<TODO[]>;
+  public doneTodo? :TODO[];
+  public undoneTodo? :TODO[];
+  public ngOnInit() {
+    this.http$ = new Observable((observe) => {
+
+      fetch('https://jsonplaceholder.typicode.com/todos')
+        .then((res) => {
+
+          return res.json();
+        }).then((body) => {
+          observe.next(body);
+          observe.complete();
+        }).catch((err) => {
+          observe.error(err);
+        })
+    });
+
+    this.http$.subscribe((val)=>{
+      this.doneTodo=val.filter(i=>i.completed==true)
+      this.undoneTodo=val.filter(i=>i.completed==false)
+    })
+  }
 
 }
---
+++ serviceTest/src/app/todo.ts
@@ -0,0 +1,6 @@
+export interface TODO {
+    id: number;
+    userId:number;
+    title:string;
+    completed:boolean;
+}
