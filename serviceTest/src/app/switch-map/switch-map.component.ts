5d8415b51ebb166f6431a245d1d7022e09c6656c
SeyedsaeidHosseini <SeyedsaeidHosseini1990@gmail.com>
2025-03-20 00:22:23 +0330

    df

--- serviceTest/src/app/switch-map/switch-map.component.ts
+++ serviceTest/src/app/switch-map/switch-map.component.ts
@@ -2,13 +2,14 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { UserserviceService } from '../userservice.service';
 import { concatAll, debounceTime, distinctUntilChanged, fromEvent, map, Observable, of, switchMap, tap } from 'rxjs';
 import { IUser } from '../user';
+import { AsyncPipe, NgFor, NgIf } from '@angular/common';
 
 
 
 
 @Component({
   selector: 'app-switch-map',
-  imports: [],
+  imports: [AsyncPipe, NgIf, NgFor],
   templateUrl: './switch-map.component.html',
   styleUrl: './switch-map.component.css'
 })
@@ -25,7 +26,8 @@ export class SwitchMapComponent implements OnInit {
       switchMap(u=>this.GetAlluser()),
       tap(i=>console.log(i))
     )
-    this.users$=of(this.GetAlluser(),usersall).pipe(concatAll());
+    this.users$=of(this.GetAlluser(),usersall).pipe(concatAll()),
+    tap(i=>console.log(i)) 
   }
   public GetAlluser(){
     return this.service.getAllUser();
