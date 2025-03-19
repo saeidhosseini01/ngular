3f29acb213dcf067211c5659608df336d256ac23
SeyedsaeidHosseini <SeyedsaeidHosseini1990@gmail.com>
2025-03-19 23:57:54 +0330

    switch

--- serviceTest/src/app/switch-map/switch-map.component.ts
+++ serviceTest/src/app/switch-map/switch-map.component.ts
@@ -1,6 +1,7 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { UserserviceService } from '../userservice.service';
-import { debounceTime, distinctUntilChanged, fromEvent, map, tap } from 'rxjs';
+import { concatAll, debounceTime, distinctUntilChanged, fromEvent, map, Observable, of, switchMap, tap } from 'rxjs';
+import { IUser } from '../user';
 
 
 
@@ -13,15 +14,18 @@ import { debounceTime, distinctUntilChanged, fromEvent, map, tap } from 'rxjs';
 })
 export class SwitchMapComponent implements OnInit {
   constructor (private service:UserserviceService ){}
+   public users$?:Observable<IUser[]>;
   @ViewChild ('termsearch',{static :true}) termSearch?:ElementRef;
   ngOnInit() {
-    fromEvent(this.termSearch?.nativeElement,'keyup').pipe(
+  const usersall=  fromEvent(this.termSearch?.nativeElement,'keyup').pipe(
       debounceTime(300),
       map(event => event as KeyboardEvent),
       map(event=>(event.target as HTMLInputElement).value),
       distinctUntilChanged(),
+      switchMap(u=>this.GetAlluser()),
       tap(i=>console.log(i))
-    ).subscribe()
+    )
+    this.users$=of(this.GetAlluser(),usersall).pipe(concatAll());
   }
   public GetAlluser(){
     return this.service.getAllUser();
